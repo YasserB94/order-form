6e49ecedf36e4fa48a336e1a9df1ecf2d82948e2
YasserB94 <yasserbal@icloud.com>
2022-06-22 16:03:10 +0200

    Added confirmation page

--- confirm.php
+++ confirm.php
@@ -1,13 +1,15 @@
 <?php ?>
-<div class="alert alert-success" role="alert">
-  <h4 class="alert-heading">Please confirm your data:</h4>
-  <?php foreach (parseUserInfo() as $userData) : ?>
-    <p><?= $userData ?></p>
-  <?php endforeach; ?>
-  <h4 class="alert-heading">Please confirm your order:</h4>
-  <?php foreach (getOrderedProducts() as $product) : ?>
-    <p><?= $product['name'] ?></p>
-  <?php endforeach; ?>
-  <button type="button" class="btn btn-success" name='confirmSuccess'>Yes, Please deliver me my order!</button>
-  <button type="button" class="btn btn-danger" name='confirmFailed'>No, I would like to make some changes</button>
-</div>
+<form method='post' action="confirmController.php">
+  <div class="alert alert-success" role="alert">
+    <h4 class="alert-heading">Please confirm your data:</h4>
+    <?php foreach (parseUserInfo() as $userData) : ?>
+      <p><?= $userData ?></p>
+    <?php endforeach; ?>
+    <h4 class="alert-heading">Please confirm your order:</h4>
+    <?php foreach (getOrderedProducts() as $product) : ?>
+      <p><?= $product['name'] ?></p>
+    <?php endforeach; ?>
+    <input type="submit" class="btn btn-success" name='confirmSuccess' value="Yes, Please deliver me my order!"></input>
+    <input type="submit" class="btn btn-danger" name='confirmFailed' value="No, I would like to make some changes"></input>
+  </div>
+</form>
